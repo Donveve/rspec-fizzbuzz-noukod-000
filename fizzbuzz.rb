c82e3d6b28d696a9068aa53a39601eaa408428e1
Veniel Dorestave <veniel@noukod.com>
2018-08-31 13:10:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-  puts  "Fizz"
-  end
+    "Fizz"
+  
 elseif int % 5 == 0
-    puts  "buzz"
-    end
-  elseif (int % 3 == 0) && (int % 5 ==0)
-      puts  "FizzBuzz"
-      end
+      "buzz"
+    
+  elseif (int % 3 == 0) && (int % 5 == 0)
+       "FizzBuzz"
+      
     else
-      "nil"
+      nil
 end
