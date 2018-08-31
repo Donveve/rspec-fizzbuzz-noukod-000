9f7fd6e58b96461a5d54e146ac9c4f2c3765caa6
Veniel Dorestave <veniel@noukod.com>
2018-08-31 13:17:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-
-elseif int % 5 == 0
+  elseif int % 5 == 0
       "buzz"
-
-  elseif (int % 3 == 0) && (int % 5 == 0)
-       "FizzBuzz"
-
-    else
+  elseif(int % 3 == 0) && (int % 5 == 0)
+      "FizzBuzz"
+  else
       "nil"
 end
